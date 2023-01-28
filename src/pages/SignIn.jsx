c25ba479dcbed90cb9f50bcf603d69ddf0365eb1
Kaiyang Yao <yaoky2008@gmail.com>
2023-01-28 17:30:48 -0600

    Enable Google OAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import OAuth from "../components/OAuth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"; // We don't need this to be a component
 
@@ -93,7 +94,7 @@ function SignIn() {
             </div>
           </form>
 
-          {/* Google OAuth */}
+          <OAuth />
 
           <Link to="/sign-up" className="registerLink">
             Sign Up Instead
